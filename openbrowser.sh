6543b1a5a10773f4fa5aeaa41e107183653f12d2
Florian Muenchbach <florian.muenchbach@student.kit.edu>
2016-07-25 18:47:25 +0200

    Changed configuration variables to use standard paths/programs.

--- openbrowser.sh
+++ openbrowser.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 URL=$1
-BROWSER=firefox
-JAVA=/usr/lib/jvm/java-8-openjdk/bin/java
-IMAGE_DOWNLOADER=/home/daniel/Projects/ImageDownloader/target/ImageDownloader-0.1.jar
-if [ ${URL: -97:1} == "#" ]; then
+BROWSER=xdg-open
+JAVA=/usr/lib/jvm/java-default-runtime/bin/java
+IMAGE_DOWNLOADER=${DIR}/target/ImageDownloader-0.1.jar
+
 	$JAVA -jar $IMAGE_DOWNLOADER $URL | feh -. -
 elif $(echo ${URL##*.} | tr '[:upper:]' '[:lower:]' | grep 'png\|jpg' &>/dev/null); then
     feh $URL
