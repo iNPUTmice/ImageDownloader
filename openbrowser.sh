cc5f4afd94b2290b5f2b89f8a3b5cb245f1e78cc
Florian Muenchbach <florian.muenchbach@student.kit.edu>
2016-07-25 18:46:08 +0200

    Added case for unencrypted image links.

--- openbrowser.sh
+++ openbrowser.sh
@@ -5,6 +5,8 @@ JAVA=/usr/lib/jvm/java-8-openjdk/bin/java
 IMAGE_DOWNLOADER=/home/daniel/Projects/ImageDownloader/target/ImageDownloader-0.1.jar
 if [ ${URL: -97:1} == "#" ]; then
 	$JAVA -jar $IMAGE_DOWNLOADER $URL | feh -. -
+elif $(echo ${URL##*.} | tr '[:upper:]' '[:lower:]' | grep 'png\|jpg' &>/dev/null); then
+    feh $URL
 else
    $BROWSER $URL
 fi
