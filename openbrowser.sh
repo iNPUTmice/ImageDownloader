1a693b4b1c287b5c4f915c074c3e216f88ac3e14
Florian Muenchbach <florian.muenchbach@student.kit.edu>
2016-07-25 18:49:04 +0200

    Fixed bash error in case link is too short.
    This would result in an empty left side of the == operator.
    With this change there will be an empty string on the left side of the operator.

--- openbrowser.sh
+++ openbrowser.sh
@@ -5,6 +5,7 @@ BROWSER=xdg-open
 JAVA=/usr/lib/jvm/java-default-runtime/bin/java
 IMAGE_DOWNLOADER=${DIR}/target/ImageDownloader-0.1.jar
 
+if [ "${URL: -97:1}" == "#" ]; then
 	$JAVA -jar $IMAGE_DOWNLOADER $URL | feh -. -
 elif $(echo ${URL##*.} | tr '[:upper:]' '[:lower:]' | grep 'png\|jpg' &>/dev/null); then
     feh $URL
